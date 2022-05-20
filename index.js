5cce59e8dcadee0b17661a534cc46294dc4efbd6
Anna Cho <loveablessing@gmail.com>
2022-05-20 00:44:25 -0400

    commit tree f54a3b3bf4d8227750b221e15eb0b988608bd213

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    let newArray = [array[0], array[1]]
+    return newArray;
+}
+const returnLastTwoDrivers = function(array){
+    let newArray = array.slice(-2)
+    return newArray;
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(){
+    return function(multiple){
+        return multiple ** 2
+    }
+}
+function fareDoubler(fare){
+    return fare * 2
+}
+function fareTripler(fare){
+    return fare * 3
+}
+function selectDifferentDrivers(arrayOfDrivers, functionName){
+    return functionName(arrayOfDrivers)
+     
+    
+}
